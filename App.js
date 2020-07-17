6432c4f56774a4b1d8879ac1866a770119653947
adengz <adengz@gmail.com>
2020-07-16 17:09:52 -0700

    feat: Make status bar colored

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StatusBar, Text } from 'react-native';
+import Constants from 'expo-constants';
+import { purple } from './utils/colors';
+
+const GeneralStatusBar = ({ backgroundColor, ...props }) => (
+  <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+    <StatusBar backgroundColor={backgroundColor} {...props} />
+  </View>
+);
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={{ flex: 1 }}>
+      <GeneralStatusBar backgroundColor={purple} barStyle="light-content" />
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ utils/colors.js
@@ -0,0 +1,6 @@
+export const white = '#FFFFFF';
+export const black = '#000000';
+export const gray = '#808080';
+export const purple = '#4B1676';
+export const green = '#4EAC5B';
+export const red = '#EB3223';
