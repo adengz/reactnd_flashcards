7431e09f5e0e2c6bd25c2272699f5bd56bc3ae87
adengz <adengz@gmail.com>
2020-07-16 17:36:00 -0700

    feat: Add tab nav

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import { View, StatusBar, Text } from 'react-native';
+import { View, StatusBar, Platform, Text } from 'react-native';
 import Constants from 'expo-constants';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import DeckList from './components/DeckList';
+import NewDeck from './components/NewDeck';
 import { purple } from './utils/colors';
 
 const GeneralStatusBar = ({ backgroundColor, ...props }) => (
@@ -9,11 +14,22 @@ const GeneralStatusBar = ({ backgroundColor, ...props }) => (
   </View>
 );
 
+const Tab = Platform.OS === 'ios' ? createBottomTabNavigator() : createMaterialTopTabNavigator();
+
+const TabNavigator = () => (
+  <Tab.Navigator>
+    <Tab.Screen name="Deck List" component={DeckList} />
+    <Tab.Screen name="New Deck" component={NewDeck} />
+  </Tab.Navigator>
+);
+
 export default function App() {
   return (
     <View style={{ flex: 1 }}>
       <GeneralStatusBar backgroundColor={purple} barStyle="light-content" />
-      <Text>Open up App.js to start working on your app!</Text>
+      <NavigationContainer>
+        <TabNavigator />
+      </NavigationContainer>
     </View>
   );
 }
---
+++ components/DeckList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const DeckList = () => (
+  <View>
+    <Text>Deck List</Text>
+  </View>
+);
+
+export default DeckList;
